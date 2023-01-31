8278d8b7ed8be3ffae295713baa18ee901519054
implicitdef <implicitdef@users.noreply.github.com>
2023-01-31 22:15:36 +0100

    refacto

--- db_cli/src/tricoteuses/tricoteusesInsertTableDeputesInLegislature.ts
+++ db_cli/src/tricoteuses/tricoteusesInsertTableDeputesInLegislature.ts
@@ -10,6 +10,16 @@ import {
 import lo from 'lodash'
 import { getDb, NosDeputesDatabase } from '../utils/db'
 import { readAutoarchiveSlugs } from '../autoarchive/autoarchiveInsert'
+import {
+  ActeurJson,
+  isMandatAssemblee,
+  isMandatComPerm,
+  isMandatGroupe,
+  MandatAssemblee,
+  readAllAssemblees,
+  readAllComPerm,
+  readAllGroupeParlementaires,
+} from '../utils/readFromTricoteuses'
 
 export async function tricoteusesInsertTableDeputesInLegislature() {
   const tableName = 'deputes_in_legislatures'
@@ -37,261 +47,122 @@ export async function tricoteusesInsertTableDeputesInLegislature() {
   await dropTable(tableName)
   await createTable(tableName, createTableSql)
 
-  const dir = path.join(WORKDIR, 'tricoteuses', AM030, 'acteurs')
-  const filenames = readFilesInSubdir(dir)
-
   const slugs = readAutoarchiveSlugs()
   const assemblees = readAllAssemblees()
   const groupes = readAllGroupeParlementaires()
   const comPerms = readAllComPerm()
 
-  const rows = filenames.flatMap(filename => {
-    const deputeJson = readFileAsJson(path.join(dir, filename)) as ActeurJson
+  const rows = readDeputesEachLegislatureAndMap(
+    (deputeJson, legislature, lastMandatThisLegislature) => {
+      const legislatureStr = legislature.toString()
+      const lastMandat = lastMandatThisLegislature
+      const gender = deputeJson.etatCivil.ident.civ === 'Mme' ? 'F' : 'M'
+      const uid = deputeJson.uid
 
-    const mandatsByLegislature = lo.groupBy(
+      const assemblee = assemblees.find(_ => _.legislature === legislatureStr)
+      if (!assemblee) {
+        throw new Error(`Didn't find assemblee ${legislatureStr}`)
+      }
+
+      const mandatsGroupeThisLegislature = deputeJson.mandats
+        .filter(isMandatGroupe)
+        .filter(_ => _.legislature === legislatureStr)
+      const lastMandatGroupe = takeLatest(mandatsGroupeThisLegislature)
+      const groupeFonction = lastMandatGroupe?.infosQualite.codeQualite
+      const groupeUid = lastMandatGroupe?.organesRefs[0]
+      const groupe = groupes.find(_ => _.uid === groupeUid)
+
+      const mandatsComPermThisLegislature = deputeJson.mandats
+        .filter(isMandatComPerm)
+        .filter(_ => _.legislature === legislatureStr)
+      const lastMandatComPerm = takeLatest(mandatsComPermThisLegislature)
+      const comPermFonction = lastMandatComPerm?.infosQualite.codeQualite
+      const comPermUid = lastMandatComPerm?.organesRefs[0]
+      const comPerm = comPerms.find(_ => _.uid === comPermUid)
+
+      const row: NosDeputesDatabase['deputes_in_legislatures'] = {
+        legislature: toInt(legislatureStr),
+        uid,
+        slug: slugs.find(_ => _.uid === uid)?.slug ?? null,
+        full_name: `${deputeJson.etatCivil.ident.prenom}_${deputeJson.etatCivil.ident.nom}`,
+        gender,
+        circo_dpt_name: lastMandat.election.lieu.departement,
+        circo_dpt_num: lastMandat.election.lieu.numDepartement,
+        circo_num: toInt(lastMandat.election.lieu.numCirco),
+        date_fin: lastMandat.dateFin ?? null,
+        group_uid: groupe?.uid ?? null,
+        group_acronym: groupe?.libelleAbrev ?? null,
+        group_fonction: groupeFonction ?? null,
+        group_color: groupe?.couleurAssociee ?? null,
+        group_pos:
+          groupe?.positionPolitique === 'Majoritaire'
+            ? 'maj'
+            : groupe?.positionPolitique === 'Minoritaire'
+            ? 'min'
+            : groupe?.positionPolitique === 'Opposition'
+            ? 'opp'
+            : null,
+        com_perm_uid: comPerm?.uid ?? null,
+        com_perm_name: comPerm?.libelleAbrev ?? null,
+        com_perm_fonction: comPermFonction ?? null,
+        ongoing:
+          lastMandat.dateFin === undefined ||
+          (assemblee.viMoDe.dateFin !== undefined &&
+            // on marque comme "ongoing" les mandats qui étaient encore en cours lorsque la législature s'est terminée
+            assemblee.viMoDe.dateFin <= lastMandat.dateFin),
+      }
+      return row
+    },
+  )
+  console.log(`Inserting ${rows.length} rows`)
+  await getDb().insertInto(tableName).values(rows).execute()
+}
+
+function readDeputesEachLegislatureAndMap<A>(
+  mapFunction: (
+    deputeJson: ActeurJson,
+    legislature: number,
+    lastMandatThisLegislature: MandatAssemblee,
+  ) => A,
+): A[] {
+  const dir = path.join(WORKDIR, 'tricoteuses', AM030, 'acteurs')
+  const filenames = readFilesInSubdir(dir)
+
+  const rows: A[] = []
+  filenames.flatMap(filename => {
+    const deputeJson = readFileAsJson(path.join(dir, filename)) as ActeurJson
+    const mandatsAssembleeByLegislature = lo.groupBy(
       deputeJson.mandats.filter(isMandatAssemblee),
       _ => _.legislature,
     )
-    if (Object.keys(mandatsByLegislature).length === 0) {
+    if (Object.keys(mandatsAssembleeByLegislature).length === 0) {
       // is not a depute
       return []
     }
     const lastMandatByLegislature = lo.mapValues(
-      mandatsByLegislature,
+      mandatsAssembleeByLegislature,
       mandats => {
         // a depute can have multiple mandats in the same legislature.
         // This could cause confusion in the very unlikely case where he has mandats in different circos
         // Lets' pick the last one
-        return lo.last(lo.sortBy(mandats, _ => _.dateFin))!
+        return takeLatest(mandats)!
       },
     )
-    const rows = Object.entries(lastMandatByLegislature).map(
+    Object.entries(lastMandatByLegislature).forEach(
       ([legislatureStr, lastMandat]) => {
-        const gender = deputeJson.etatCivil.ident.civ === 'Mme' ? 'F' : 'M'
-        const uid = deputeJson.uid
-
-        const assemblee = assemblees.find(_ => _.legislature === legislatureStr)
-        if (!assemblee) {
-          throw new Error(`Didn't find assemblee ${legislatureStr}`)
-        }
-
-        const mandatsGroupeThisLegislature = deputeJson.mandats
-          .filter(isMandatGroupe)
-          .filter(_ => _.legislature === legislatureStr)
-        const lastMandatGroupe = lo.last(
-          lo.sortBy(mandatsGroupeThisLegislature, _ => _.dateFin),
-        )
-        const groupeFonction = lastMandatGroupe?.infosQualite.codeQualite
-        const groupeUid = lastMandatGroupe?.organesRefs[0]
-        const groupe = groupes.find(_ => _.uid === groupeUid)
-
-        const mandatsComPermThisLegislature = deputeJson.mandats
-          .filter(isMandatComPerm)
-          .filter(_ => _.legislature === legislatureStr)
-        const lastMandatComPerm = lo.last(
-          lo.sortBy(mandatsComPermThisLegislature, _ => _.dateFin),
-        )
-        const comPermFonction = lastMandatComPerm?.infosQualite.codeQualite
-        const comPermUid = lastMandatComPerm?.organesRefs[0]
-        const comPerm = comPerms.find(_ => _.uid === comPermUid)
-
-        const row: NosDeputesDatabase['deputes_in_legislatures'] = {
-          legislature: toInt(legislatureStr),
-          uid,
-          slug: slugs.find(_ => _.uid === uid)?.slug ?? null,
-          full_name: `${deputeJson.etatCivil.ident.prenom}_${deputeJson.etatCivil.ident.nom}`,
-          gender,
-          circo_dpt_name: lastMandat.election.lieu.departement,
-          circo_dpt_num: lastMandat.election.lieu.numDepartement,
-          circo_num: toInt(lastMandat.election.lieu.numCirco),
-          date_fin: lastMandat.dateFin ?? null,
-          group_uid: groupe?.uid ?? null,
-          group_acronym: groupe?.libelleAbrev ?? null,
-          group_fonction: groupeFonction ?? null,
-          group_color: groupe?.couleurAssociee ?? null,
-          group_pos:
-            groupe?.positionPolitique === 'Majoritaire'
-              ? 'maj'
-              : groupe?.positionPolitique === 'Minoritaire'
-              ? 'min'
-              : groupe?.positionPolitique === 'Opposition'
-              ? 'opp'
-              : null,
-          com_perm_uid: comPerm?.uid ?? null,
-          com_perm_name: comPerm?.libelleAbrev ?? null,
-          com_perm_fonction: comPermFonction ?? null,
-          ongoing:
-            lastMandat.dateFin === undefined ||
-            (assemblee.viMoDe.dateFin !== undefined &&
-              // on marque comme "ongoing" les mandats qui étaient encore en cours lorsque la législature s'est terminée
-              assemblee.viMoDe.dateFin <= lastMandat.dateFin),
-        }
-        return row
+        const row = mapFunction(deputeJson, toInt(legislatureStr), lastMandat)
+        rows.push(row)
       },
     )
-    return rows
   })
-
-  console.log(`Inserting ${rows.length} rows`)
-
-  await getDb().insertInto(tableName).values(rows).execute()
+  return rows
 }
 
-function readAllAssemblees(): OrganeAssemblee[] {
-  const dir = path.join(WORKDIR, 'tricoteuses', AM030, 'organes')
-  const filenames = readFilesInSubdir(dir)
-  const res = filenames.flatMap(filename => {
-    const organeJson = readFileAsJson(path.join(dir, filename)) as OrganeJson
-    if (isOrganeAssemblee(organeJson)) {
-      return [organeJson]
-    }
-    return []
-  })
-  return res
-}
-
-function readAllGroupeParlementaires(): OrganeGroupe[] {
-  const dir = path.join(WORKDIR, 'tricoteuses', AM030, 'organes')
-  const filenames = readFilesInSubdir(dir)
-  const res = filenames.flatMap(filename => {
-    const organeJson = readFileAsJson(path.join(dir, filename)) as OrganeJson
-    if (isOrganeGroupe(organeJson)) {
-      return [organeJson]
-    }
-    return []
-  })
-  return res
-}
-
-function readAllComPerm(): OrganeComPerm[] {
-  const dir = path.join(WORKDIR, 'tricoteuses', AM030, 'organes')
-  const filenames = readFilesInSubdir(dir)
-  const res = filenames.flatMap(filename => {
-    const organeJson = readFileAsJson(path.join(dir, filename)) as OrganeJson
-    if (isOrganeComPerm(organeJson)) {
-      return [organeJson]
-    }
-    return []
-  })
-  return res
-}
-
-type ActeurJson = {
-  uid: string
-  etatCivil: {
-    ident: { civ: 'M.' | 'Mme'; nom: string; prenom: string }
-  }
-  mandats: Mandat[]
-  // there are other fields
-}
-
-type Mandat =
-  | MandatAssemblee
-  | MandatGroupe
-  | MandatComPerm
-  | {
-      typeOrgane: '__other__'
-    }
-
-type MandatAssemblee = {
-  typeOrgane: 'ASSEMBLEE'
-  legislature: string
-  election: {
-    lieu: {
-      departement: string
-      numDepartement: string
-      numCirco: string
-    }
-  }
-  dateFin?: string
-}
-
-type MandatGroupe = {
-  typeOrgane: 'GP'
-  legislature: string
-  dateFin?: string
-  infosQualite: {
-    codeQualite: FonctionInGroupe
-  }
-  organesRefs: [string]
-}
-
-type MandatComPerm = {
-  typeOrgane: 'COMPER'
-  legislature: string
-  dateFin?: string
-  infosQualite: {
-    codeQualite: FonctionInCom
-  }
-  organesRefs: [string]
-}
-
-type FonctionInGroupe =
-  | 'Président'
-  | 'Membre apparenté'
-  | 'Membre'
-  | 'Député non-inscrit'
-
-type FonctionInCom =
-  | 'Président'
-  | 'Membre'
-  | 'Rapporteur général'
-  | 'Secrétaire'
-  | 'Vice-Président'
-
-function isMandatAssemblee(mandat: Mandat): mandat is MandatAssemblee {
-  return mandat.typeOrgane === 'ASSEMBLEE'
-}
-
-function isMandatGroupe(mandat: Mandat): mandat is MandatGroupe {
-  return mandat.typeOrgane === 'GP'
-}
-
-function isMandatComPerm(mandat: Mandat): mandat is MandatComPerm {
-  return mandat.typeOrgane === 'COMPER'
-}
-
-type OrganeJson =
-  | OrganeAssemblee
-  | OrganeGroupe
-  | OrganeComPerm
-  | {
-      codeType: '__other__'
-    }
-
-type OrganeAssemblee = {
-  uid: string
-  codeType: 'ASSEMBLEE'
-  legislature: string
-  viMoDe: { dateFin?: string }
-}
-
-type OrganeGroupe = {
-  uid: string
-  codeType: 'GP'
-  legislature: string
-  libelleAbrev: string
-  couleurAssociee?: string
-  positionPolitique?: 'Majoritaire' | 'Minoritaire' | 'Opposition'
-}
-
-type OrganeComPerm = {
-  uid: string
-  codeType: 'COMPER'
-  libelleAbrege: string
-  libelleAbrev: string
-}
-
-function isOrganeAssemblee(organe: OrganeJson): organe is OrganeAssemblee {
-  return organe.codeType === 'ASSEMBLEE'
-}
-
-function isOrganeGroupe(organe: OrganeJson): organe is OrganeGroupe {
-  return organe.codeType === 'GP'
-}
-
-function isOrganeComPerm(organe: OrganeJson): organe is OrganeComPerm {
-  return organe.codeType === 'COMPER'
+// Given an array objects with a dateFin,
+// will return the one where dateFin is the latest
+// An undefined value for dateFin is considered as the latest
+function takeLatest<A extends { dateFin?: string }>(arr: A[]): A | undefined {
+  return lo.last(lo.sortBy(arr, _ => _.dateFin))
 }
 
 function toInt(s: string) {
---
+++ db_cli/src/utils/readFromTricoteuses.ts
@@ -0,0 +1,150 @@
+import path from 'path'
+import { AM030 } from './tricoteusesDatasets'
+import { readFileAsJson, readFilesInSubdir, WORKDIR } from './utils'
+
+function readOrganesAndFilter<Subtype extends OrganeJson>(
+  filterFunction: (o: OrganeJson) => o is Subtype,
+): Subtype[] {
+  const dir = path.join(WORKDIR, 'tricoteuses', AM030, 'organes')
+  const filenames = readFilesInSubdir(dir)
+  const res = filenames.flatMap(filename => {
+    const organeJson = readFileAsJson(path.join(dir, filename)) as OrganeJson
+    if (filterFunction(organeJson)) {
+      return [organeJson]
+    }
+    return []
+  })
+  return res
+}
+
+export function readAllAssemblees(): OrganeAssemblee[] {
+  return readOrganesAndFilter(isOrganeAssemblee)
+}
+
+export function readAllGroupeParlementaires(): OrganeGroupe[] {
+  return readOrganesAndFilter(isOrganeGroupe)
+}
+
+export function readAllComPerm(): OrganeComPerm[] {
+  return readOrganesAndFilter(isOrganeComPerm)
+}
+
+export type ActeurJson = {
+  uid: string
+  etatCivil: {
+    ident: { civ: 'M.' | 'Mme'; nom: string; prenom: string }
+  }
+  mandats: Mandat[]
+  // there are other fields
+}
+
+export type Mandat =
+  | MandatAssemblee
+  | MandatGroupe
+  | MandatComPerm
+  | {
+      typeOrgane: '__other__'
+    }
+
+export type MandatAssemblee = {
+  typeOrgane: 'ASSEMBLEE'
+  legislature: string
+  election: {
+    lieu: {
+      departement: string
+      numDepartement: string
+      numCirco: string
+    }
+  }
+  dateFin?: string
+}
+
+export type MandatGroupe = {
+  typeOrgane: 'GP'
+  legislature: string
+  dateFin?: string
+  infosQualite: {
+    codeQualite: FonctionInGroupe
+  }
+  organesRefs: [string]
+}
+
+export type MandatComPerm = {
+  typeOrgane: 'COMPER'
+  legislature: string
+  dateFin?: string
+  infosQualite: {
+    codeQualite: FonctionInCom
+  }
+  organesRefs: [string]
+}
+
+export type FonctionInGroupe =
+  | 'Président'
+  | 'Membre apparenté'
+  | 'Membre'
+  | 'Député non-inscrit'
+
+export type FonctionInCom =
+  | 'Président'
+  | 'Membre'
+  | 'Rapporteur général'
+  | 'Secrétaire'
+  | 'Vice-Président'
+
+export function isMandatAssemblee(mandat: Mandat): mandat is MandatAssemblee {
+  return mandat.typeOrgane === 'ASSEMBLEE'
+}
+
+export function isMandatGroupe(mandat: Mandat): mandat is MandatGroupe {
+  return mandat.typeOrgane === 'GP'
+}
+
+export function isMandatComPerm(mandat: Mandat): mandat is MandatComPerm {
+  return mandat.typeOrgane === 'COMPER'
+}
+
+export type OrganeJson =
+  | OrganeAssemblee
+  | OrganeGroupe
+  | OrganeComPerm
+  | {
+      codeType: '__other__'
+    }
+
+export type OrganeAssemblee = {
+  uid: string
+  codeType: 'ASSEMBLEE'
+  legislature: string
+  viMoDe: { dateFin?: string }
+}
+
+export type OrganeGroupe = {
+  uid: string
+  codeType: 'GP'
+  legislature: string
+  libelleAbrev: string
+  couleurAssociee?: string
+  positionPolitique?: 'Majoritaire' | 'Minoritaire' | 'Opposition'
+}
+
+export type OrganeComPerm = {
+  uid: string
+  codeType: 'COMPER'
+  libelleAbrege: string
+  libelleAbrev: string
+}
+
+export function isOrganeAssemblee(
+  organe: OrganeJson,
+): organe is OrganeAssemblee {
+  return organe.codeType === 'ASSEMBLEE'
+}
+
+export function isOrganeGroupe(organe: OrganeJson): organe is OrganeGroupe {
+  return organe.codeType === 'GP'
+}
+
+export function isOrganeComPerm(organe: OrganeJson): organe is OrganeComPerm {
+  return organe.codeType === 'COMPER'
+}
