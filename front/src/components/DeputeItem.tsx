22c9f6b78f3cd16e479f02e36ff3e45be78be1cb
implicitdef <implicitdef@users.noreply.github.com>
2023-02-01 20:36:32 +0100

    as tags

--- front/src/components/DeputeItem.tsx
+++ front/src/components/DeputeItem.tsx
@@ -81,7 +81,7 @@ export function DeputeItem({
         {latestComPerm &&
           latestComPerm.fonction !== 'Membre' &&
           latestComPerm.name_short && (
-            <div className="h-1/2 w-full bg-slate-400 px-2 italic text-slate-200">
+            <div className=" mx-1 mb-1  bg-slate-400 px-1 italic text-slate-200">
               {' '}
               {latestComPerm.fonction} com.{' '}
               {getComPermNameWithPrefix(latestComPerm.name_short)}
